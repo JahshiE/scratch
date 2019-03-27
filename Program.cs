d66d1487d1287968c1d296b683dfed01a326dd29
Josh <joshua.espinas@gmail.com>
2019-03-27 14:19:45 +1300

    Has the structure of a basic user definted exception class

--- Program.cs
+++ Program.cs
@@ -6,7 +6,41 @@ namespace scratch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine("My example of creating a user defined exception:\n");
+
+            var myNumber = new DivideByZero();
+
+            try
+            {
+                myNumber.isNotZero();
+            }
+            catch (DivideByZeroException e)
+            {
+                System.Console.WriteLine($"DivideByZeroException: {e.Message}");
+            }
+        }
+    }
+}
+
+public class DivideByZeroException : Exception
+{
+    public DivideByZeroException(string message) : base(message)
+    {
+    }
+}
+
+public class DivideByZero
+{
+    int number = 0;
+
+    public void isNotZero() {
+        if (number == 0)
+        {
+            throw (new DivideByZeroException("Number was zero! Can't divide."));
+        }
+        else
+        {
+            System.Console.WriteLine($"Number: {number}");
         }
     }
 }
