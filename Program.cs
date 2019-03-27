3f8eaf0b4ecb42ab761fbdd31634b58c741d0093
Josh <joshua.espinas@gmail.com>
2019-03-28 10:15:47 +1300

    Changed to Number class, and convert input from string to int

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace scratch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("My example of creating a user defined exception:\n");
+            System.Console.Write("Give me a number between 5 and 10: ");
+            var input = Console.ReadLine();
+            System.Console.WriteLine($"You have given me {input}");
+            var myNumber = new Number(Convert.ToInt32(input));
+
+            try
+            {
+                myNumber.canBeDivided();
+            }
+            catch (DivideByZeroException e)
+            {
+                System.Console.WriteLine($"DivideByZeroException: {e.Message}");
+            }
+        }
+    }
+}
+
+public class DivideByZeroException : Exception
+{
+    public DivideByZeroException(string message) : base(message)
+    {
+    }
+}
+
+public class Number
+{
+    private int _value;
+
+    public Number(int value)
+    {
+        _value = value;
+    }
+
+    public void canBeDivided() {
+        if (_value == 0)
+        {
+            throw (new DivideByZeroException("Number was zero! Can't divide."));
+        }
+        else
+        {
+            System.Console.WriteLine($"Yes, {_value} can be divided because it is not zero.");
+        }
+    }
+}
